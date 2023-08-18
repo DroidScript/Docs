7e8b4cd9498acff798631f0bcb81ebf886de471c
hamacjumar <hamacjumar@gmail.com>
2023-08-18 12:48:29 +0800

    Added _base.js file as a starting point for adding base methods

---
+++ files/markup/en/app/_base.js
@@ -0,0 +1,8 @@
+
+/** ### Animate
+ * Animates the control. *  * There are * 	“in”-Animations which are used to show objects from hidden state * 	“out”-animations which are used to hide objects in visible state and * 	“static”-animations which keep the visible state.
+ * $$ btn.Animate(type, callback, time) $$
+ * @param {str} type NewsPaper|Jelly|Flash|RubberBand|Swing|TaDa|Bounce|Fall|FallRotate|<br><img width='300px' src='../animate.png'></img>
+ * @param {fnc_json} callback {"pNames":["type"],"pTypes":["str"]}
+ * @param {num_mls} time
+ */
