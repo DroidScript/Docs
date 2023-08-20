6d6b7bce448518df3ab55dbf7810647b05cdb5b8
hamacjumar <hamacjumar@gmail.com>
2023-08-20 15:52:36 +0800

    Remove base folder

--- files/markup/en/base/gfx/_AddBasicSprite.js
+++
@@ -1,8 +0,0 @@
-
-/** # _AddBasicSprite #
- * Adds a [Basic Sprite](CreateBasicSprite.htm) to your game.
- * $$ gfx._AddBasicSprite(sprite) $$ 
- * @param {gvo} sprite CreateSprite
-*/
-
-
--- files/markup/en/base/gfx/_SetPixelMode.js
+++
@@ -1,8 +0,0 @@
-
-/** # _SetPixelMode #
- * 
- * $$ gfx._SetPixelMode(usePixels) $$ 
- * @param {bin} usePixels 
-*/
-
-
