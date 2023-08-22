2a990b8e6a2cd0a3f881d38c51aeb1abf5beed4a
alex-Symbroson <alex.symbroson@gmail.com>
2023-08-22 20:46:07 +0200

    better dev scripts & README

--- files/README.md
+++ files/README.md
@@ -8,8 +8,15 @@
 Install dependencies
 - `npm i` 
 
-Run generation pipeline (json, docs)
-- `npm run generate` 
+Generation Pipelines
+- `npm run generate` generate files/out/docs from json
+- `npm run generateDocs` generate displayed docs from json
+- `npm run updateVersion` generate displayed docs and updates version
 
-Run individual pipeline step
-- `npm run [json|docs|markup]` 
+Individual pipeline steps
+- `npm run docs` generate files/out/docs from json
+- `npm run docs-lazy` docs, but regenerates only outdated scopes
+- `npm run update` docs-lazy, but updates files/out/version.txt
+- `npm run updatePages` update displayed docs (copy out/ to docs/)
+- `npm run json` convert markup files to json
+- `npm run markup` convert json files to markup
---
+++ files/updatePages.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const path = require('path');
+const fs = require('fs-extra');
+const rimraf = require('rimraf').sync;
+const glob = require('glob/sync')
+
+main();
+
+function main() {
+    const base = path.join(__dirname, '..') + path.sep;
+
+    const version = fs.readFileSync(base + "files/version.txt", 'utf8').slice(-3);
+    for (const file of glob(path.join(base, 'files/out/*/*/Docs.htm'))) {
+        const content = fs.readFileSync(file, 'utf8');
+        fs.writeFileSync(file, content.replace(/Docs version:\ [0-9]*/, `Docs version: ${version}`));
+    }
+
+    console.log("removing old docs");
+    try { rimraf(base + "docs"); } catch(e) { console.error(e.message); }
+
+    console.log("copying new docs");
+    fs.mkdirSync(base + "docs");
+    fs.copy(base + "files/out", base + "docs");
+    fs.copy(base + "files/version.txt", base + "docs/version.txt");
+
+    console.log('done');
+}
