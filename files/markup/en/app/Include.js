c2f8714c4436718ac429b68a6e6680a4a0dcbe21
J.Hamac <48899694+hamacjumar@users.noreply.github.com>
2024-11-23 19:37:07 +0800

    Create Include.js

---
+++ files/markup/en/app/Include.js
@@ -0,0 +1,14 @@
+// ------------- HEADER SECTION -------------
+
+
+/** # Include #
+ * @brief import an external JavaScript file
+ * Load an external javascript file (prefered over Script method).
+ * $$ app.Include( file ) $$
+ * @param {str_ptf} file
+*/
+
+
+// ------------- LONG DESCRIPTION -------------
+See Also: @Script
+ */
