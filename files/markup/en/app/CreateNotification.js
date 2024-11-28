c0cd048a940f3b6d93e8dd30cd3adc6181ed47b3
J.Hamac <48899694+hamacjumar@users.noreply.github.com>
2024-11-28 11:44:27 +0800

    Update spelling of "notification" from "notificatin"

--- files/markup/en/app/CreateNotification.js
+++ files/markup/en/app/CreateNotification.js
@@ -14,7 +14,7 @@
 // ------------- LONG DESCRIPTION -------------
 
 /** @Description
-The Notification object can be used to put messages in the notification drawer. The “AutoCancel” **option** will automatically dismiss the notificatin if the user touches it. “FullScreen” tries to create a larger notification on the screen. And “OnGoing” makes the Notification closeable by your app only.
+The Notification object can be used to put messages in the notification drawer. The “AutoCancel” **option** will automatically dismiss the notification if the user touches it. “FullScreen” tries to create a larger notification on the screen. And “OnGoing” makes the Notification closeable by your app only.
 
 Use the SetMessage method to tell the object what to display
 <js>notify.SetMessage( ticker, title, text );</js>
